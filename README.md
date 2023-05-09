023f9f588120de4b12cc4e6a0e31a8147c82f8f3
wendellsantos2 <wemdellcos@gmail.com>
2023-05-09 19:57:54 -0300

    alterações

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Javascript_Alura
 Curso de javascript 
+https://cursos.alura.com.br/course/javascript-web-paginas-dinamicas/task/97748
--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-alert('ola mundo');
+function tocaSom (seletorAudio) {
+   const elemento = document.querySelector(seletorAudio);
+   if(elemento === null){
+    alert('elemento nao encontrado')
+   }
+   if(elemento != ){
+    
+   }
+}
+ 
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+ 
+for (let contador = 0 ; contador < listaDeTeclas.length; contador++ ){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+   //template string
+   const idAudio =`#som_${instrumento}`
+ 
+   
+   tecla.onclick = function(){
+       tocaSom(idAudio);
+    } 
+    
+    tecla.onkeydown = function(evento){
+    
+        if (evento.code === 'Space' || evento.code=== 'Enter'){
+            tecla.classList.add('ativa');
+        }
+ 
+        
+    }
+ 
+    tecla.onkeyup = function (){
+        tecla.classList.remove('ativa');
+    }
+}
